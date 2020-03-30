a1f1a0ef60c5560b44191caca5e2925c5075c6c8
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-03-30 13:29:43 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component{
     }
 
     componentDidMount(){
-        fetch(" http://api.open-notify.org/astros.json")
+        fetch("http://api.open-notify.org/astros.json")
             .then(resp => resp.json())
             .then(json => this.setState({people: json.people}))
             // .then(this.listPeople())
